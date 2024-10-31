b4b1c7704ce7657ccca42b9655b5a5510885e341
facuforipa <facufolatelli2001@gmail.com>
2024-10-31 16:34:25 -0300

    pa tras

--- examples/server.js
+++ examples/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
-
+const path = require('path');
 const app = express();
 const PORT = 3012;
 
-// Cargar los certificados SSL
-const options = {
-    key: fs.readFileSync('./private.key'), // Reemplaza con la ruta a tu archivo de clave privada
-    cert: fs.readFileSync('./certificate.crt')   // Reemplaza con la ruta a tu certificado
-};
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static('public'));
 
-// Iniciar el servidor HTTPS
-https.createServer(options, app).listen(PORT, () => {
-    console.log(`Servidor escuchando en https://149.50.131.9:${PORT}`);
+// Ruta para la página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
