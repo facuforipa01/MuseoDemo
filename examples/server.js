86711cc1e8e561d6386f1be7bce321c1e166b816
facuforipa <facufolatelli2001@gmail.com>
2024-10-31 15:10:18 -0300

    transformado en server

---
+++ examples/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3011;
+
+// Servir archivos estáticos desde la carpeta "public"
+app.use(express.static('public'));
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
